8fe13f88711db8f88089ce15d9faff328f92cf30
Valentin Sauer <valentin@sauer-medientechnik.de>
2016-06-25 11:40:02 +0200

    fixed the scope mess

--- www/js/write.js
+++ www/js/write.js
@@ -19,10 +19,12 @@ angular.module('hooru.write', [])
             });
         }
 
-        $scope.write = "Test";
+        $scope.write = {};
+        $scope.write.text = "Test";
+        $scope.step = 0;
 
         $scope.preparestep = function(step){
-            console.log("PREPARING");
+
             // Prepare for next step
             hooru.resetProfiledata();
             $scope.step = step;
@@ -40,7 +42,7 @@ angular.module('hooru.write', [])
                 $scope.read = hooru.texts[step];
             }
             $scope.incomplete = true;
-            $scope.write = "asd";
+            $scope.write.text = "";
         }
 
         if ($state.current.name == 'write') {
@@ -73,17 +75,15 @@ angular.module('hooru.write', [])
                     hooru.upload();
                     if ($scope.step == 0) {
                         $scope.preparestep(1);
-                        //this.write = "";
                     } else if ($scope.step == 1) {
                         $scope.preparestep(2);
-                        // this.write = "";
                     } else if ($scope.step == 2) {
                         $state.go('calc');
                         hooru.resetProfiledata();
-                        this.write = "";
+                        $scope.write.text = "";
                     }
                 } else {
-
+                    $scope.write.text = "";
                     var trainingpopup = $ionicPopup.alert({
                         title: "Arbeite.",
                         template: '<p>Hooru versucht dich nun anhand des trainierten neuronalen Netzes zu identifizieren...</p><div class="center"><ion-spinner icon="spiral"></ion-spinner></div>',
@@ -97,28 +97,29 @@ angular.module('hooru.write', [])
                             'Content-Type': undefined
                         }
                     }
+
+                    $http(req).then(function (response) {
+                        // success
+                        if (response.data) {
+                            var net = new brain.NeuralNetwork();
+                            net.fromJSON(JSON.parse(response.data));
+                            var mydata = hooru.getNormalizedProfileData();
+                            var output = false;
+                            var output = net.run(mydata);
+                            hooru.setOutput(output);
+                            trainingpopup.close();
+                            $scope.preparestep('identify');
+                            $state.go('result');
+                        }
+                    }, function () {
+                        console.log("Download error");
+                    });
                 }
 
-                $http(req).then(function (response) {
-                    // success
-                    if (response.data) {
-                        var net = new brain.NeuralNetwork();
-                        net.fromJSON(JSON.parse(response.data));
-                        var mydata = hooru.getNormalizedProfileData();
-                        var output = false;
-                        var output = net.run(mydata);
-                        hooru.setOutput(output);
-                        trainingpopup.close();
-                        $state.go('result');
-                    }
-                }, function () {
-                    console.log("Download error");
-                });
             } else if (write.length == hooru.texts[$scope.step].length && hooru.getError(hooru.texts[$scope.step], write) > 0) {
                 $ionicPopup.alert({
                     title: "Fehlerhafte Eingabe",
                     template: 'Du hast bei der Eingabe zu viele Rechtschreibfehler gemacht. Bitte wiederhole die Eingabe.',
-                    scope: $scope,
                     buttons: [
                         {
                             text: '<b>OK</b>',
